5c7eedf33c232cac74eaa8297d79b09bf1a79e22
vituscokr <vituscokr@gmail.com>
2023-02-22 12:20:14 +0900

    앱 아이콘 만들기

---
+++ appIconGenerate.sh
@@ -0,0 +1,27 @@
+cp  1024.png 40.png
+cp  1024.png 60.png
+cp  1024.png 58.png
+cp  1024.png 87.png
+cp  1024.png 80.png
+cp  1024.png 120.png
+cp  1024.png 180.png
+cp  1024.png 20.png
+cp  1024.png 29.png
+cp  1024.png 80.png
+cp  1024.png 76.png
+cp  1024.png 152.png
+cp  1024.png 167.png
+
+sips -Z 40 40.png
+sips -Z 60 60.png
+sips -Z 58 58.png
+sips -Z 87 87.png
+sips -Z 80 80.png
+sips -Z 120 120.png
+sips -Z 180 180.png
+sips -Z 20 20.png
+sips -Z 29 29.png
+sips -Z 80 80.png
+sips -Z 76 76.png
+sips -Z 152 152.png
+sips -Z 167 167.png
